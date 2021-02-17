afa5fc036019be786379c2d4cba0916500176045
Randi Schreiner <mimirandi21@gmail.com>
2021-02-17 12:13:46 -0600

    commit tree 12a8d0939f9a217999b84cdaee6ab658bea4f545

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+let initialState = [];
+export function manageFriends(state = initialState, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { ...state, friends: [...state.friends, action.friend] };
+    case "REMOVE_FRIEND":
+      break;
+    default:
+      return state;
+      break;
+  }
 }
